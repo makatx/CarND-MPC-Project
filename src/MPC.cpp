f34fa1e72e87593f23fb2642c201f2358b66b8be
Mak <mak@khan.com>
2018-07-14 16:40:31 +0300

    Update README

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -7,7 +7,7 @@ using CppAD::AD;
 
 // TODO: Set the timestep length and duration
 size_t N = 10;
-double dt = 0.05;
+double dt = 0.1;
 
 // This value assumes the model presented in the classroom is used.
 //
@@ -20,7 +20,7 @@ double dt = 0.05;
 //
 // This is the length from front to CoG that has a similar radius.
 const double Lf = 2.67;
-double ref_v = 35;
+double ref_v = 100;
 
 // The solver takes all the state variables and actuator
 // variables in a singular vector. Thus, we should to establish
@@ -63,12 +63,14 @@ class FG_eval {
     // TODO: Define the cost related the reference state and
     // any anything you think may be beneficial.
     for(size_t i=0; i<N; i++) {
-      fg[0] += CppAD::pow(vars[cte_start+i], 2)/500 ;
+      fg[0] += CppAD::pow(vars[cte_start+i], 2)/500.0 ;
       fg[0] += CppAD::pow(vars[epsi_start+i], 2);
-      fg[0] += CppAD::pow(vars[v_start+i]-ref_v, 2);
+      //fg[0] += CppAD::pow(vars[v_start+i]-ref_v, 2);
+
     }
     for(size_t t=0; t<N-1; t++) {
       fg[0] += CppAD::pow(vars[delta_start + t], 2) + CppAD::pow(vars[a_start + t], 2);
+      //fg[0] += CppAD::pow(vars[v_start+t]*(vars[delta_start+t]), 2);
     }
     for (size_t t = 0; t < N - 2; t++) {
       fg[0] += 30*CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
@@ -124,7 +126,7 @@ class FG_eval {
 
       // TODO: Setup the rest of the model constraints
       fg[1 + x_start + t] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
-      fg[1 + y_start + t] = y1 - (y0 + v0 * CppAD::cos(psi0) * dt);
+      fg[1 + y_start + t] = y1 - (y0 + v0 * CppAD::sin(psi0) * dt);
       fg[1 + psi_start + t] = psi1 - (psi0 - v0 * delta0 * dt / Lf);
       fg[1+ v_start + t] = v1 - (v0 + a0 * dt);
       fg[1 + cte_start + t] =
